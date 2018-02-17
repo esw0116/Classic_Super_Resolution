196fadab4992edc22edc07ef8c952d07c3ca5a0a
esw0116 <esw0116@gmail.com>
2018-02-18 01:26:00 +0900

    train / test alternatively

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from test import TEST
 with tf.Session() as sess:
     channel_length = 1
     save_path = './save_model/one_image'
-
+    '''
     train_one_image = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path)
-    train_one_image.train(iteration=1000)
-
+    train_one_image.train(iteration=10000)
+    '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
     infer_one_image.test()
