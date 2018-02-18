7672f2510727c44cd1b25fcd299cb3c4c7ee1ea1
esw0116 <esw0116@gmail.com>
2018-02-19 06:45:37 +0900

    minor change

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ with tf.Session() as sess:
     channel_length = 1
     save_path = './save_model/one_image'
     '''
-    train_one_image = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path)
-    train_one_image.train(iteration=10000)
+    train_one_image = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=True)
+    train_one_image.train(iteration=50000)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
     infer_one_image.test()
