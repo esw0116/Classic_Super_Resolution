f18ec79a3c80f2c7c8add82f9cf9f39c8ea38790
esw0116 <esw0116@gmail.com>
2018-02-19 22:42:52 +0900

    works on a single GPU

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import tensorflow as tf
 from train import TRAIN
 from test import TEST
 
-with tf.Session() as sess:
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+with tf.Session(config = config) as sess:
     channel_length = 1
     save_path = './save_model/one_image'
-    '''
-    train_one_image = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=True)
+    
+    train_one_image = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False)
     train_one_image.train(iteration=50000)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
     infer_one_image.test()
+    '''
