7b52eaa4ca100d97068a89d39bc278ca4b9ab5a8
esw0116 <esw0116@gmail.com>
2018-02-20 14:35:29 +0900

    train <-> test

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ config.gpu_options.allow_growth = True
 with tf.Session(config = config) as sess:
     channel_length = 1
     save_path = './save_model/gray_dataset'
-    '''
+
     train_one_image = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False, patch_size=33, num_patch_per_image=20)
-    train_one_image.train(iteration=50000)
+    train_one_image.train(iteration=10000)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
     infer_one_image.test(inference=True)
-
+    '''
