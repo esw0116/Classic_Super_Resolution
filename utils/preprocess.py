8cee278c3721ca61045bb3c57428f6784563118f
esw0116 <esw0116@gmail.com>
2018-02-20 11:06:41 +0900

    add make patch

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -3,9 +3,29 @@ import os
 import glob
 from PIL import Image
 
-def load_data(path):
-    data_list = glob.glob(path, '*.png')
 
+def load_list(path):
+    data_list = sorted(glob.glob(path, '*.png'))
+    return data_list
+
+
+def load_data(list, start, end, patch_size, num_patch_per_image):
+    # start~ (end-1)
+    imagepatch = np.empty([num_patch_per_image * (end - start), patch_size, patch_size], dtype='float32')
+    for i in range(start, end):
+        #load image, extract Y component
+        temp_image = np.array(Image.open(list[i]))
+        # temp_image = temp_image[:, :, 0]
+
+        w = temp_image.shape[0]
+        h = temp_image.shape[1]
+        rand_x = np.random.randint(w - patch_size, size=num_patch_per_image)
+        rand_y = np.random.randint(h - patch_size, size=num_patch_per_image)
+
+        for j in range(num_patch_per_image):
+            temp_patch = temp_image[rand_x[j]:rand_x[j]+patch_size, rand_y[j]:rand_y[j]+patch_size]
+            imagepatch[num_patch_per_image*(i-start)+j, :, :] = temp_patch
+    return imagepatch[:, :, :, np.newaxis]
 
 def convert_btw_rgb_ycbcr(image, dir):
     if dir:  # rgb 2 ycbcr
