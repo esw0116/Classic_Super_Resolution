91623929e2b8e2546f947045fa5cf4e0219188b9
esw0116 <esw0116@gmail.com>
2018-02-20 14:21:52 +0900

    edited for the set5 dataset

--- test.py
+++ test.py
@@ -1,31 +1,28 @@
 import tensorflow as tf
 import numpy as np
+import glob
 from PIL import Image
 from model import SRCNN
 from utils import preprocess
 
 class TEST:
     def __init__(self, sess, channel_length, save_path):
-        # self.image_size = image_size
-        # self.label_size = label_size
         self.c_length = channel_length
-        self.x = tf.placeholder(dtype='float32', shape=[None, 525, 680, self.c_length], name='image')
-        self.y = tf.placeholder(dtype='float32', shape=[None, 525, 680, self.c_length], name='image')
+        self.x = tf.placeholder(dtype='float32', shape=[None, None, None, self.c_length], name='image')
+        self.y = tf.placeholder(dtype='float32', shape=[None, None, None, self.c_length], name='image')
         self.save_path = save_path
         if sess is not None:
             self.sess = sess
 
-    def test(self):
+    def test(self, inference):
         # images = low resolution, labels = high resolution
         sess = self.sess
 
         # for training a particular image(one image)
-        image, label = preprocess.image_label_gen(image_path='sample/house_low.png', label_path='sample/house.png')
-        image_y = image[:, :, 0]
+        test_image_list = glob.glob('./dataset/test/gray_low/*.*')
+        test_label_list = glob.glob('./dataset/test/gray/*.*')
 
-        image_y = image_y[np.newaxis, :, :, np.newaxis]
-        label_y = label[:, :, 0]
-        label_y = label_y[np.newaxis, :, :, np.newaxis]
+        num_image = len(test_image_list)
 
         # img size = label size = 525 * 680
 
@@ -41,23 +38,18 @@ class TEST:
         saver = tf.train.Saver()
 
         saver.restore(sess, self.save_path)
+        for i in range(num_image):
+            test_image = np.array(Image.open(test_image_list[i]))
+            test_image = test_image[np.newaxis, :, :, np.newaxis]
+            test_label = np.array(Image.open(test_label_list[i]))
+            test_label = test_label[np.newaxis, :, :, np.newaxis]
 
-        final_psnr = sess.run(psnr, feed_dict={self.x: image_y, self.y: label_y})
+            final_psnr = sess.run(psnr, feed_dict={self.x: test_image, self.y: test_label})
 
-        print('Test PSNR is ', final_psnr)
+            print('Test PSNR is ', final_psnr)
 
-        pred_y = sess.run(prediction, feed_dict={self.x: image_y, self.y: label_y})
-        pred_y = np.squeeze(pred_y).astype(dtype='uint8')
-        image_cb = image[:, :, 1]
-        image_cr = image[:, :, 2]
-
-        original =np.stack((image_y, image_cb, image_cr), axis=-1)
-        orig_img = Image.fromarray(original, mode='YCbCr')
-        orig_img.show()
-
-        restore = np.stack((pred_y, image_cb, image_cr), axis=-1)
-        print(restore.shape)
-        img = Image.fromarray(restore, mode='YCbCr')
-        img.show()
-        img2 = img.convert('RGB')
-        img2.save('house_sr.png')
+            if inference:
+                pred = sess.run(prediction, feed_dict={self.x: test_image, self.y: test_label})
+                pred = np.squeeze(pred).astype(dtype='uint8')
+                pred_image = Image.fromarray(pred)
+                pred_image.show()
