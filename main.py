cb74967613243e28e0f5fbf64a1c47c2922f04ba
esw0116 <esw0116@gmail.com>
2018-02-21 10:15:34 +0900

    support VDSR

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ config.gpu_options.allow_growth = True
 with tf.Session(config = config) as sess:
     channel_length = 1
     save_path = './save_model/vdsr/20170220'
-
+    '''
     train_images = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False, patch_size=33, num_patch_per_image=20)
     train_images.train_vdsr(iteration=3000)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
-    infer_one_image.test(inference=True)
-    '''
+    infer_one_image.test(mode='VDSR', inference=True)
