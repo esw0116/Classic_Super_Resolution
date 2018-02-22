90c90d0c5dec01a4da694531beb7ceb41d917cb9
Su Young Lee <esw0116@gmail.com>
2018-02-22 18:38:56 +0900

    mode changed to SRCNN

--- main.py
+++ main.py
@@ -14,10 +14,9 @@ with tf.Session(config=config) as sess:
     args = parser.parse_args()
     save_path = args.save_path
     # save_path = './save_model/20170222/srcnn'
-
+    '''
     train_images = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False, patch_size=33, num_patch_per_image=20)
     train_images.train_srcnn(iteration=10000)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
-    infer_one_image.test(mode='VDSR', inference=True)
-    '''
+    infer_one_image.test(mode='SRCNN', inference=True)
