eecf99f58fac3565c06ff325dc560db443a45321
Su Young Lee <esw0116@gmail.com>
2018-02-22 18:38:40 +0900

    SGD ->ADAM

--- train.py
+++ train.py
@@ -33,10 +33,10 @@ class TRAIN:
         v1, v2, prediction = sr_model.build_model()
 
         with tf.name_scope("mse_loss"):
-            loss = 0.5 * tf.reduce_mean(tf.square(self.y - prediction))
+            loss = tf.reduce_mean(tf.square(self.y - prediction))
 
-        train_op1 = tf.train.GradientDescentOptimizer(learning_rate=1e-4).minimize(loss, var_list=v1)
-        train_op2 = tf.train.GradientDescentOptimizer(learning_rate=1e-5).minimize(loss, var_list=v2)
+        train_op1 = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, var_list=v1)
+        train_op2 = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss, var_list=v2)
         train_op = tf.group(train_op1, train_op2)
 
         # optimize = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(loss)
