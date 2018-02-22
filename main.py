e9246f9cbf59c3ba2f87e333884a2417f69c3913
Su Young Lee <esw0116@gmail.com>
2018-02-23 02:36:42 +0900

    change patch size, # of patches per image

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ with tf.Session(config=config) as sess:
     save_path = args.save_path
     # save_path = './save_model/20170222/srcnn'
     
-    train_images = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False, patch_size=41, num_patch_per_image=20)
-    train_images.train_vdsr(iteration=80)
+    train_images = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False, patch_size=33, num_patch_per_image=20)
+    train_images.train_srcnn(iteration=15000)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
     infer_one_image.test(mode='SRCNN', inference=True)
