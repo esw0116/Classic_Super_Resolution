8efa4e21b8d8ae338b7e14655c2f799ab2e224ec
Su Young Lee <esw0116@gmail.com>
2018-03-01 22:32:13 +0900

    X2, X3, X4 data augmentation

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ with tf.Session(config=config) as sess:
     # save_path = './save_model/20170222/srcnn'
     
     train_images = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False, patch_size=33, num_patch_per_image=20)
-    train_images.train_srcnn(iteration=15000)
+    train_images.train_vdsr(iteration=100)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
     infer_one_image.test(mode='SRCNN', inference=True)
