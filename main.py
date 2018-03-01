7ba17133d9595c82ba566854472631b2950696fc
Su Young Lee <esw0116@gmail.com>
2018-03-01 22:43:14 +0900

    model name with argparse

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ with tf.Session(config=config) as sess:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_path')
+    parser.add_argument('--model')
     args = parser.parse_args()
     save_path = args.save_path
+    model = args.model
     # save_path = './save_model/20170222/srcnn'
     
     train_images = TRAIN(sess=sess, channel_length=channel_length, save_path=save_path, pre_trained=False, patch_size=33, num_patch_per_image=20)
-    train_images.train_vdsr(iteration=100)
+    if model == 'srcnn':
+        train_images.train_srcnn(iteration=5000)
+    elif model == 'vdsr':
+        train_images.train_vdsr(iteration=100)
     '''
     infer_one_image = TEST(sess=sess, channel_length=channel_length, save_path=save_path)
     infer_one_image.test(mode='SRCNN', inference=True)
