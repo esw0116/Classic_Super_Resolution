1e562fa35d14cdc137a23785fd78d9c0ba9acc05
Su Young Lee <esw0116@gmail.com>
2018-03-04 23:51:36 +0900

    adam

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ class TRAIN:
         train_op2 = tf.train.GradientDescentOptimizer(learning_rate=1e-5).minimize(loss, var_list=v2)
         train_op = tf.group(train_op1, train_op2)
         '''
-        train_op = tf.train.GradientDescentOptimizer(learning_rate=1e-4).minimize(loss)
+        train_op = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
 
         batch_size = 3
         num_batch = int(num_image/batch_size)
